d295f67225462bddfa4b3c86243d02fd3ab517cb
alebombini <alejandrabombi@hotmail.com>
2023-11-04 17:29:52 -0300

    queries mobile tickets

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let email = document.getElementById("email");
 let cantidad = document.getElementById("cantidad");
 let categoria = document.getElementById("categoria");
 
-
+/*
 //validacion de datos de nombre y email
 const validarDatos = () => {
     const nombreInput = document.getElementById('nombre').value;
@@ -35,21 +35,27 @@ const validarDatos = () => {
                 emailValido = true;
             }
 }
-
+*/
 
 //funcion para calcular el total en OnClick
 const calcularTotal = (categoria, cantidad) => {
-    let cantidad = document.getElementById("cantidad").value;
-    let categoria = document.getElementById("categoria").value;
+    //let cantidad = document.getElementById("cantidad").value;
+    //let categoria = document.getElementById("categoria").value;
     if (categoria === "Estudiante"){
         let total = 40 * cantidad;
+        document.write("<p>" + total + "</p>");
+        alert("categoria "+ categoria + "cantidad " + cantidad);
     }else if (categoria === "Trainee"){
         let total = 100 * cantidad;
+        document.write("<p>" + total + "</p>");
+        alert("categoria "+ categoria + "cantidad " + cantidad);    
     }else if (categoria === "Junior"){
         let total = 70 * cantidad;
-    } else if (categoria === ""){
+        document.write("<p>" + total + "</p>");
+        alert("categoria "+ categoria + "cantidad " + cantidad);
+    } else if (categoria === "general"){
         let total = 200 * cantidad;
+        document.write("<p>" + total + "</p>");
+        alert("categoria "+ categoria + "cantidad " + cantidad);
     }return total;
-    document.write("<p>" + total + "</p>");
-    alert("categoria "+ categoria + "cantidad " + cantidad);
 }
