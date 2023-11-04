64a066230420a6e1b48afe9b9342bdd46f353786
alebombini <alejandrabombi@hotmail.com>
2023-11-04 17:04:06 -0300

    pagina tickets

---
+++ script.js
@@ -0,0 +1,55 @@
+//declaro todos los input en variables
+let general         = 200;
+let descuentoEstudiante = 80;
+let descuentoTrainee    = 50;
+let descuentoJunior     = 15;
+
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+let email = document.getElementById("email");
+let cantidad = document.getElementById("cantidad");
+let categoria = document.getElementById("categoria");
+
+
+//validacion de datos de nombre y email
+const validarDatos = () => {
+    const nombreInput = document.getElementById('nombre').value;
+    const apellidoInput = document.getElementById("apellido").value;
+    const emailInput = document.getElementById('email').value;
+    let nombreValido = false;
+    let emailValido = false;
+    let apellidoValido = false;
+    if (nombreInput.value === ''){
+        alert("Por favor, ingrese su nombre");
+        } else {
+            nombreValido = true;
+            }
+            if (apellidoInput.value === ''){
+                alert("Por favor, ingrese su nombre");
+                } else {
+                    nombreValido = true;
+                    }        
+    if (emailInput.value === ''){
+                alert("Ingrese un email válido");
+            } else {
+                emailValido = true;
+            }
+}
+
+
+//funcion para calcular el total en OnClick
+const calcularTotal = (categoria, cantidad) => {
+    let cantidad = document.getElementById("cantidad").value;
+    let categoria = document.getElementById("categoria").value;
+    if (categoria === "Estudiante"){
+        let total = 40 * cantidad;
+    }else if (categoria === "Trainee"){
+        let total = 100 * cantidad;
+    }else if (categoria === "Junior"){
+        let total = 70 * cantidad;
+    } else if (categoria === ""){
+        let total = 200 * cantidad;
+    }return total;
+    document.write("<p>" + total + "</p>");
+    alert("categoria "+ categoria + "cantidad " + cantidad);
+}
