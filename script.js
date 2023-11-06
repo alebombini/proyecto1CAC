3794f0fce8923bd3b3fc99ec0fece7d0a4af5ad3
alebombini <alejandrabombi@hotmail.com>
2023-11-06 07:12:59 -0300

    corrijo errores

--- script.js
+++ script.js
@@ -20,28 +20,23 @@ const quitarClaseError = () => {
         listaNodos[i].classList.remove('is-invalid')
     }
     let listaNodosdiv = document.querySelectorAll(".invalid-feedback");
-    for (let i = 0; i < listaNodosdiv.length; i++) {
-        listaNodosdiv[i].classList.remove("mostrarError");
-    }
+
 }
 
 const totalAPagar = () => {
     quitarClaseError();
     if(nombre.value === "") {
         nombre.classList.add("is-invalid");
-        divErrorNombre.classList.add("mostrarError");
         nombre.focus();
         return;
     }
     if(apellido.value === ""){
         apellido.classList.add("is-invalid");
-        divErrorApellido.classList.add("mostrarError");
         apellido.focus();
         return;
     }
     if (mail.value === ""){
         mail.classList.add("is-invalid");
-        divErrorEmail.classList.add("mostrarError");
         mail.focus();
         return;
     }
@@ -50,19 +45,16 @@ const totalAPagar = () => {
     }
     if (!emailValido(mail.value)) {
         mail.classList.add("is-invalid");  
-        divErrorEmail.classList.add("mostrarError");
         mail.focus();
         return;
     }
     if ((cantidadTickets.value < 1) || (isNaN(cantidadTickets.value))) {
         cantidadTickets.classList.add("is-invalid");
-        divErrorCantidad.classList.add("mostrarError");
         cantidadTickets.focus();
         return;
     }
     if (categoria.value == "") {
         categoria.classList.add("is-invalid");
-        divErrorCategorias.classList.add("mostrarError");
         categorias.focus();
         return;
     }
